e3ff084f2df5294eece77550478899b3be22b45c
Jin Tao <taojin1992@users.noreply.github.com>
2020-06-03 22:30:53 -0700

    Create 24_SwapNodesinPairs.java

---
+++ W1_LinkedList/more/24_SwapNodesinPairs.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode nextStart = null;
+        while (head != null && head.next != null) {
+            nextStart = head.next.next;
+            pre.next = head.next;
+            head.next.next = head;
+            pre = head;
+            head.next = nextStart;            
+            head = nextStart;
+        }
+        return dummy.next;
+    }
+}
